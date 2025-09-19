a39b7641cea4ce1001405edbb168fd66dd3f6949
Nebil Gokdemir <32441666+Nebilsolomon@users.noreply.github.com>
2025-09-18 21:04:14 -0700

    add BoxTraceSingle when sword hit enemy

--- Source/Portfolio2/Weapon.cpp
+++ Source/Portfolio2/Weapon.cpp
@@ -7,6 +7,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "Components/SphereComponent.h"
 #include "Components/BoxComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 
 
@@ -14,6 +15,18 @@
 AWeapon::AWeapon() {
 	WeaponBox = CreateDefaultSubobject<UBoxComponent>(TEXT("WeaponBox"));
 	WeaponBox->SetupAttachment(GetRootComponent());
+	WeaponBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	WeaponBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	WeaponBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+
+
+	StartTraceBox = CreateDefaultSubobject<USceneComponent>(TEXT("StartTraceBox"));
+	StartTraceBox->SetupAttachment(GetRootComponent());
+	EndTraceBox = CreateDefaultSubobject<USceneComponent>(TEXT("EndTraceBox"));
+	EndTraceBox->SetupAttachment(GetRootComponent());
+
+
+
 
 
 }
@@ -24,7 +37,7 @@ void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 
-
+	WeaponBox->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnBoxOverlapBegin);
 
 }
 
@@ -52,7 +65,38 @@ void AWeapon::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherAct
 
 
 
+void AWeapon::OnBoxOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
 
+
+	const FVector Start = StartTraceBox->GetComponentLocation();
+	const FVector End = EndTraceBox->GetComponentLocation();
+	 DrawDebugLine(GetWorld(), Start, End, FColor::Red, false, 2.f);
+
+
+
+	 TArray<AActor*> ActorToIgnore;
+	 ActorToIgnore.Add(this);
+	 FHitResult BoxHit;
+	 UKismetSystemLibrary::BoxTraceSingle(
+		 this,
+		 Start,
+		 End,
+		 FVector(5, 5, 5),
+		 StartTraceBox->GetComponentRotation(),
+		 ETraceTypeQuery::TraceTypeQuery1,
+		 false,
+		 ActorToIgnore,
+		 EDrawDebugTrace::ForDuration,
+		 BoxHit,
+		 true
+
+
+
+	 );
+
+
+
+}
 void AWeapon::Equip(class ANinja* Char, FName SocketName) {
 	if (Char) {
 		// Attach the weapon to the character's hand
@@ -79,3 +123,7 @@ void AWeapon::Equip(class ANinja* Char, FName SocketName) {
 
 	}
 }
+
+
+
+
--- Source/Portfolio2/Weapon.h
+++ Source/Portfolio2/Weapon.h
@@ -26,6 +26,11 @@ public:
 
 	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 	
+	UFUNCTION()
+
+	virtual void OnBoxOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Sound")
 
 	class USoundBase* EquipSound;
@@ -33,6 +38,13 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item")
 
 	class UBoxComponent* WeaponBox;
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Trace1")
+	USceneComponent* StartTraceBox;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Trace2")
+	USceneComponent* EndTraceBox;
 
 	
 };
